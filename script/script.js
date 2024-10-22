e8b7265fb38b92fe31e6ff561c4d21cba47336c7
emarkees <emarkees@gmail.com>
2024-10-22 12:49:16 +0100

    Add header and sideNav script

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const hamburger = document.getElementById('hamburger');
+const menuIcon = document.getElementById('menu-icon');
+const navLinksContainer = document.querySelector('.nav-links-container');
+
+
+//Function to toggle the menu and sidebar
+function toggleMenu() {
+  navLinksContainer.classList.toggle('active');
+  if (hamburger.classList.toggle('active')) {
+    menuIcon.src = './Assets/Images/hamburger.svg'
+  } else {
+    menuIcon.src = './Assets/Images/Home.png';
+  }
+};
+
+// Add click event listener to the hamburger menu
+hamburger.addEventListener('click', toggleMenu);
+
+document.querySelectorAll('nav-menu')
+.forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navLinksContainer.classList.remove('active')
+}));
+
